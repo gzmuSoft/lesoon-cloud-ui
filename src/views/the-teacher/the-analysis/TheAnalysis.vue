a84b1eebf4534ebed48a9df32c3ef3b99702b74a
lxr <Lxinrui10@163.com>
2019-06-18 19:36:09 +0800

    统计分析首页

--- src/views/the-teacher/the-analysis/TheAnalysis.vue
+++ src/views/the-teacher/the-analysis/TheAnalysis.vue
@@ -124,6 +124,39 @@
           span.lesson-teacher-analysis-information 查看&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;考试设置&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;编辑试题&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;考试统计&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;考试延期
         br
 
+  <!--.lesson-teacher-analysis-exam-->
+    <!--P.lesson-teacher-analysis-exam-title 考试统计-->
+    <!--span.lesson-teacher-analysis-exam-course 课程名称：2019第二学期面向对象程序设计考试-->
+    <!--span.lesson-teacher-analysis-exam-persons 统计人数：50人-->
+    <!--br-->
+    <!--Tabs-->
+      <!--TabPane(label="考试成绩情况分析")-->
+        <!--Card.lesson-teacher-analysis-exam-grade-->
+          <!--Form(label-position="left" :label-width="75")-->
+            <!--Row(:gutter="13" type="flex" justify="space-around" class="code-row-bg")-->
+              <!--Col(span="5")-->
+                <!--FormItem.lesson-teacher-analysis-exam-studentsName(label="学生姓名：")-->
+                  <!--Input(size="large" placeholder="请输入学生姓名")-->
+              <!--Col(span="5")-->
+                <!--FormItem.lesson-teacher-analysis-exam-group(label="小组：")-->
+                  <!--Select-->
+                    <!--Option 全部-->
+              <!--Col(span="5")-->
+                <!--Button(type="primary") 查询-->
+              <!--Col(span="4")-->
+                <!--Button(type="primary")  按小组导出-->
+                <!--Button(type="primary")  导出全部-->
+
+          <!--Table(ref='tables', editable, addable :loading='loadingFlag' v-model='tableData', :columns='columns',-->
+          <!--@on-delete='handleDelete',-->
+          <!--@on-save-edit='handleSave',-->
+          <!--@on-start-edit='handleEdit',-->
+          <!--@on-cancel-edit='handleCancel',-->
+          <!--@on-add='handleAdd')-->
+
+      <!--TabPane(label="学生答题分析")-->
+      <!--TabPane(label="答题内容分析")-->
+
 </template>
 
 <script>
@@ -141,6 +174,34 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  /*.lesson-teacher-analysis-exam{*/
+    /*margin-left: 80px;*/
+  /*}*/
+  /*.lesson-teacher-analysis-exam-title{*/
+    /*font-size: 18px;*/
+    /*margin-left: -895px;*/
+  /*}*/
+  /*.lesson-teacher-analysis-exam-course{*/
+    /*margin-left: -495px;*/
+    /*font-size: 14px;*/
+    /*color: darkgray;*/
+  /*}*/
+  /*.lesson-teacher-analysis-exam-persons{*/
+    /*margin-left: 60px;*/
+    /*font-size: 14px;*/
+    /*color: darkgray;*/
+  /*}*/
+  /*.lesson-teacher-analysis-exam-grade{*/
+    /*margin-top: 400px;*/
+  /*}*/
+  /*.lesson-teacher-analysis-exam-studentsName{*/
+    /*width: 200px;*/
+    /*margin-left: 10px;*/
+  /*}*/
+  /*.lesson-teacher-analysis-exam-group{*/
+    /*margin-left: 10px;*/
+    /*width: 200px;*/
+  /*}*/
   .lesson-teacher-analysis-tabs{
     margin-left: 80px;
   }
